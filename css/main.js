058084bcd97f917f7bcd015532d6a10a84475b84
syedkhadri0521-hue <syedkhadri0521@gmail.com>
2026-05-13 00:44:30 +0530

    first commit

--- css/main.js
+++ css/main.js
@@ -1,7 +1,35 @@
 const themeToggle = document.getElementById("theme-toggle");
 
-themeToggle.addEventListener("click", () => {
+/* CHECK BUTTON */
 
-  document.body.classList.toggle("dark-mode");
+if(themeToggle){
 
-});
+  /* LOAD SAVED THEME */
+
+  if(localStorage.getItem("theme") === "dark"){
+
+    document.body.classList.add("dark-mode");
+
+  }
+
+  /* TOGGLE THEME */
+
+  themeToggle.addEventListener("click", () => {
+
+    document.body.classList.toggle("dark-mode");
+
+    /* SAVE THEME */
+
+    if(document.body.classList.contains("dark-mode")){
+
+      localStorage.setItem("theme","dark");
+
+    }else{
+
+      localStorage.setItem("theme","light");
+
+    }
+
+  });
+
+}
