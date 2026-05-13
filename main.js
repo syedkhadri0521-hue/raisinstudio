c9620eeb70ad83c08afeb3bc3709e919edf30a8b
syedkhadri0521-hue <syedkhadri0521@gmail.com>
2026-05-13 10:23:03 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,29 @@
+// LUCIDE ICONS
+
+lucide.createIcons();
+
+
+// ======================
+// DARK MODE
+// ======================
+
+const darkModeBtn = document.querySelector(".dark-mode-btn");
+
+darkModeBtn.addEventListener("click", () => {
+
+  document.body.classList.toggle("dark-mode");
+
+});
+
+
+// ======================
+// RTL MODE
+// ======================
+
+const rtlBtn = document.querySelector(".rtl-btn");
+
+rtlBtn.addEventListener("click", () => {
+
+  document.body.classList.toggle("rtl-mode");
+
+});
