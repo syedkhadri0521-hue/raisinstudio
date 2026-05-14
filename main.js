ab86c22184ef19066ca79c8816a266d75f00940e
syedkhadri0521-hue <syedkhadri0521@gmail.com>
2026-05-14 17:41:55 +0530

    first commit

--- main.js
+++ main.js
@@ -27,3 +27,34 @@ rtlBtn.addEventListener("click", () => {
   document.body.classList.toggle("rtl-mode");
 
 });
+
+/* ===================================================
+   DARK MODE
+=================================================== */
+
+const themeToggle = document.getElementById("theme-toggle");
+
+/* CLICK */
+
+themeToggle.addEventListener("click", () => {
+
+  document.body.classList.toggle("dark-mode");
+
+  /* SAVE THEME */
+
+  if(document.body.classList.contains("dark-mode")){
+
+    localStorage.setItem("theme","dark");
+
+  }else{
+
+    localStorage.setItem("theme","light");
+  }
+});
+
+/* LOAD SAVED THEME */
+
+if(localStorage.getItem("theme") === "dark"){
+
+  document.body.classList.add("dark-mode");
+}
